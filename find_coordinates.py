b5c866f6506e393b8d47718d8ded15be717a3842
markbutler94 <markbutler94@gmail.com>
2015-09-14 23:32:11 +0100

    Added explanation

--- find_coordinates.py
+++ find_coordinates.py
@@ -1,8 +1,10 @@
+# This is a tool for finding coordinates of territories. 
+# Load a map here, then click to print the position of the mouse click to the console.
+
 def findCoords():
     win = GraphWin("MAP",1000,669)
-    i = Image(Point(500,669/2),"C:\\Users\\Mark\\Google Drive\\Risk\\testmap.gif")
+    i = Image(Point(500,669/2),"C:\\Users\\Mark\\Google Drive\\Risk\\maps\\mapOLD.gif")
     i.draw(win)
 
     while 1>0:
         print(win.getMouse().x,win.getMouse().y)
-
